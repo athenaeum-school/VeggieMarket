b63e77792815b1fc656e80f789ddb66529fe86a1
inotaku <i_no_u_u@yahoo.co.jp>
2014-12-20 14:47:48 +0900

    Spring Integration追加

---
+++ VeggieMarketApp/Shipping/src/main/java/jp/com/inotaku/domain/Blog.java
@@ -0,0 +1,14 @@
+package jp.com.inotaku.domain;
+
+import lombok.Data;
+
+@Data
+public class Blog {
+
+	private String __v;
+	private String title;
+	private String message;
+	private String _id;
+	private String customer;
+	private String shipping;
+}
